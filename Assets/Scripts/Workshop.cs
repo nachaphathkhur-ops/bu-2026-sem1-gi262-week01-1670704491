fe86754f5bb34e418ffba65a629d648875c916dc
KaKrxn <itthichai.faen@bumail.net>
2026-08-18 16:44:04 +0700

    Workshop

--- Assets/Scripts/Workshop.cs
+++ Assets/Scripts/Workshop.cs
@@ -1,3 +1,5 @@
+using Newtonsoft.Json;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Workshop : MonoBehaviour
@@ -18,31 +20,66 @@ public class Workshop : MonoBehaviour
     public bool isSixOClock;
     public void As01_SyntaxIf()
     {
-        
+        if (isSixOClock)
+        {
+            Debug.Log("The door open");
+        }
+        Debug.Log("Knock Knock");
     }
 
     public string password;
     public void As02_StringComparisonExample()
     {
+        if (password == "moon") {
+            Debug.Log("password is correct");
+        }
+
+        if (password != "moon")
+        {
+            Debug.Log("password is incorrect");
+        }
+
         
     }
 
     public int as03Number;
     public void As03_NumberComparisonExample()
     {
-
+        if (as03Number == 10)
+        {
+            Debug.Log("My number == 10");
+        }
+        if (as03Number != 10) 
+        {
+            Debug.Log("My number != 10");
+        }
     }
 
     public int as04Number;
     public void As04_AndOrOperatorExample()
     {
-
+        if (as04Number > 8 && as04Number < 12)
+        {
+            Debug.Log("My number 8 > < 12");
+        }
+        if(as04Number < 8 || as04Number > 12)
+        {
+            Debug.Log("My number 8 || 12");
+        }
     }
 
     public int as05GuessingNumber;
     public int as05RandomNumber;
     public void As05_GuessingNumberExample()
     {
+        if (as05GuessingNumber == as05RandomNumber)
+        {
+            Debug.Log("congratulation");
+        }
+        else
+        {
+            Debug.Log("try agin");
+        }
 
     }
 
@@ -50,6 +87,20 @@ public class Workshop : MonoBehaviour
     public int as06RandomNumber;
     public void As06_GuessingNumberMoreOrLessExample()
     {
+        if (as06GuessingNumber < as06RandomNumber)
+        {
+            Debug.Log("Too Low");
+        }
+        else if (as06GuessingNumber > as06RandomNumber)
+        {
+            Debug.Log("Too high");
+        }
+        else
+        {
+            Debug.Log("Congratulation");
+        }
+       
+        
 
     }
 
@@ -59,6 +110,26 @@ public class Workshop : MonoBehaviour
     public bool as07IsPaid;
     public void As07_VerifyIdentityExample()
     {
+        if (as07Username == "user" && as07Password == "user123")
+        {
+            Debug.Log("you have accessed");
+            if (as07IsPaid)
+            {
+                Debug.Log("VIP member");
+            }
+            else
+            {
+                Debug.Log("Free member");
+            }
+
+            if (as07Age >= 18)
+            {
+                Debug.Log("You have access to exclusive content");
+            }
+        }
+        else {
+            Debug.Log("You are guest");
+        }
 
     }
 }
